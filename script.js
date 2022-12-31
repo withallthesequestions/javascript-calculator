215477de00b1a766bb78b2ced6b2f86d255c0c4a
withallthesequestions <uytpic434516@gmail.com>
2022-12-30 20:14:10 -0500

    First commit

---
+++ script.js
@@ -0,0 +1,68 @@
+/* Experiments
+
+// Testing display update
+let varName1 = document.getElementsByClassName("displayWindow")[0];
+varName1.innerText = "testing"; 
+
+// Log keypress to console
+document.addEventListener("click", function (event) {
+  console.log(event.target.value);
+}); 
+
+// Log keypress to display
+document.addEventListener("click", function (event) {
+  document.getElementsByClassName("displayWindow")[0].innerText =
+    event.target.value;
+}); */
+
+/* Logic
+  b) Only buttons can be clicked (This is done by making sure only buttons are covered by button classes (mathValues = all numbers and operators, button value "=" = the equals button, which evaluates the string, value = "AC", and value = "CE")
+  f) After you're done running an eval, you want your next click to start a brand-new operation.*/
+
+let inputString = "";
+let displayString = "";
+let lastButtonPressed = "";
+
+// Event-listener, listening for button-clicks
+document.addEventListener("click", function (event) {
+  lastButtonPressed = event.target.value;
+  // Filling in the inputString with your math equation
+  // Note: The preferred condition is event.target.value, but we want to pick out a whole class of math buttons, so we're going with "event.target.classList.contains("mathValues")" instead.
+  if (event.target.classList.contains("mathValues")) {
+    fillInputString(event);
+  }
+  // Bug: When you press "=" twice, the calculation outputs "undefined". This is because the inputString is emptied. Fix: Only calculate if inputString is not empty (ie, && inputString !== "").
+  if (event.target.value === "=" && inputString !== "") {
+    calculate();
+  }
+  if (event.target.value === "AC") {
+    clearEverything();
+  }
+  console.log(inputString);
+});
+
+// Fill inputString Function
+function fillInputString(event) {
+  inputString += event.target.value;
+  displayString = inputString;
+  document.getElementsByClassName("displayWindow")[0].innerText = displayString;
+}
+
+// Equals-Button Function
+function calculate() {
+  //1.f.
+  // The following three lines let you smoothly transition into a new math calculation after the last result is output. (1) The displayString variable lets you keep a value in the window while working on other buttons. (2) The inputString lets you perform subsequent calculations on the result of the original operation.
+  displayString = eval(inputString);
+  inputString = eval(inputString);
+  document.getElementsByClassName("displayWindow")[0].innerText = displayString;
+}
+
+// AC-Button Function
+function clearEverything() {
+  displayString = "0";
+  inputString = "";
+  lastButtonPressed = "";
+  document.getElementsByClassName("displayWindow")[0].innerText = displayString;
+}
+
+/* To-do: Implement CE (clear entry) */
